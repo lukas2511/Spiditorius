f93666c6925c084350777c6d50c14083f0669565
Lukas Schauer <lukas2511@xxpro.net>
2012-11-12 22:12:39 +0100

    things

--- inc/Sprites.h
+++ inc/Sprites.h
@@ -1,6 +1,10 @@
 #ifndef __SPRITES_H__
 #define __SPRITES_H__
 #include <RLEBitmap.h>
+extern const RLEBitmap *const convert_u_anim_1;
+extern const RLEBitmap *const convert_u_anim_2;
+extern const RLEBitmap *const convert_u_anim_3;
+extern const RLEBitmap *const convert_u_anim_4;
 extern const RLEBitmap *const spider;
 extern const RLEBitmap *const spider_d_anim_1;
 extern const RLEBitmap *const spider_d_anim_2;
