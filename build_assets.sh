dc5cb8704eea74a738bc8c6577d522aad22d1569
Lukas Schauer <lukas2511@xxpro.net>
2012-11-12 21:36:02 +0100

    things

--- build_assets.sh
+++ build_assets.sh
@@ -2,9 +2,20 @@
 
 echo '#include <RLEBitmap.h>' > src/Sprites.c
 echo >> src/Sprites.c
+
+echo '#ifndef __SPRITES_H__' > inc/Sprites.h
+echo '#define __SPRITES_H__' >> inc/Sprites.h
+echo '#include <RLEBitmap.h>' >> inc/Sprites.h
+
 for asset in assets/*.png
 do
 	name="$(basename $asset | cut -d. -f1)"
+
 	PNGConverter $asset RLEBitmap $name >> src/Sprites.c
 	echo >> src/Sprites.c
+
+	echo 'extern const RLEBitmap *const '$name >> inc/Sprites.h
 done
+
+echo '#endif' >> inc/Sprites.h
+
