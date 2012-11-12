f246864a125b70c0d4475c1f5586808c314b005f
Lukas Schauer <lukas2511@xxpro.net>
2012-11-12 21:02:56 +0100

    bla

--- src/Game.c
+++ src/Game.c
@@ -19,6 +19,9 @@ void Draw(Bitmap *);
 Gamestate InitState = { Init, OnEnter, OnLeave, Update, Draw };
 Game* TheGame = &(Game) {&InitState};
 
+int x=5;
+int y=5;
+
 void Init(struct Gamestate* state)
 {
 }
@@ -33,11 +36,41 @@ void OnLeave(struct Gamestate* state)
 
 void Update(uint32_t a)
 {
+    snes_button_state_t controller_state = GetControllerState1();
+    if(controller_state.buttons.Up){
+        y--;
+    }
+    if(controller_state.buttons.Down){
+        y++;
+    }
+    if(controller_state.buttons.Left){
+        x--;
+    }
+    if(controller_state.buttons.Right){
+        x++;
+    }
+
+    if(x<-8) x=320+x;
+    if(y<-8) y=200+y;
+    if(x>320) x=-8;
+    if(y>200) y=-8;
 }
 
 void Draw(Bitmap *b)
 {
     ClearBitmap(b);
 
-    DrawRLEBitmap(b, spider, 5, 10);
+    if(x<0){
+        DrawRLEBitmap(b, spider, 320+x, y);
+    }
+    if(y<0){
+        DrawRLEBitmap(b, spider, x, 200+y);
+        if(x<0){
+            DrawRLEBitmap(b, spider, 320+x, 200+y);
+        }
+    }
+    if(x>312){
+        DrawRLEBitmap(b, spider, -(320-x), y);        
+    }
+    DrawRLEBitmap(b, spider, x, y);
 }
