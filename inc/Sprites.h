c2c66477d6e621bccf8e6590b610be40fe206611
Stefan <stefan@potatopowered.net>
2012-11-29 18:10:36 +0100

    added bolt

--- inc/Sprites.h
+++ inc/Sprites.h
@@ -1,6 +1,7 @@
 #ifndef __SPRITES_H__
 #define __SPRITES_H__
 #include <RLEBitmap.h>
+extern const RLEBitmap *const bolt;
 extern const RLEBitmap *const spider_d_anim_1;
 extern const RLEBitmap *const spider_d_anim_2;
 extern const RLEBitmap *const spider_d_anim_3;
@@ -26,4 +27,20 @@ extern const RLEBitmap *const wasp_r0;
 extern const RLEBitmap *const wasp_r1;
 extern const RLEBitmap *const wasp_u0;
 extern const RLEBitmap *const wasp_u1;
+extern const RLEBitmap *const worm00;
+extern const RLEBitmap *const worm01;
+extern const RLEBitmap *const worm02;
+extern const RLEBitmap *const worm03;
+extern const RLEBitmap *const worm10;
+extern const RLEBitmap *const worm11;
+extern const RLEBitmap *const worm12;
+extern const RLEBitmap *const worm13;
+extern const RLEBitmap *const worm20;
+extern const RLEBitmap *const worm21;
+extern const RLEBitmap *const worm22;
+extern const RLEBitmap *const worm23;
+extern const RLEBitmap *const worm30;
+extern const RLEBitmap *const worm31;
+extern const RLEBitmap *const worm32;
+extern const RLEBitmap *const worm33;
 #endif
